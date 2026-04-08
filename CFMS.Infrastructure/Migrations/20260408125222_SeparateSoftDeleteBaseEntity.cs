c44eea1cae3763ae83542863e109314cdc65b069
sirajhasand <sirajuddinhasand@gmail.com>
2026-04-08 17:26:38 +0430

    new change

---
+++ CFMS.Infrastructure/Migrations/20260408125222_SeparateSoftDeleteBaseEntity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CFMS.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeparateSoftDeleteBaseEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
