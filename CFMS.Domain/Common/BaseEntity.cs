b9528c7a1f6dd71f31b4f9af9b3b315c5a6bc2bf
sirajhasand <sirajuddinhasand@gmail.com>
2026-04-08 17:26:09 +0430

    new change

--- CFMS.Domain/Common/BaseEntity.cs
+++ CFMS.Domain/Common/BaseEntity.cs
@@ -1,9 +1,19 @@
 namespace CFMS.Domain.Common;
 
+/// <summary>
+/// Base entity with common properties for ALL entities
+/// </summary>
 public abstract class BaseEntity
 {
-    public Guid Id { get; set; } = Guid.NewGuid();  
+    public Guid Id { get; set; } = Guid.NewGuid();
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime? UpdatedAt { get; set; }
-    public bool IsDeleted { get; set; } = false;  // not for all should be create sprt
+}
+
+/// <summary>
+/// Base entity with soft delete functionality - use only for entities that need deletion
+/// </summary>
+public abstract class SoftDeleteBaseEntity : BaseEntity
+{
+    public bool IsDeleted { get; set; } = false;
 }
